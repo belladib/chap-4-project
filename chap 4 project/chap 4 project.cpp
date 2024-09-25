d49b6c31035f7f10e8b9ae92bede27086fe4bc63
baebe <baebe@FELICITY>
2024-09-25 03:33:11 -0400

    it's getting real.

--- chap 4 project/chap 4 project.cpp
+++ chap 4 project/chap 4 project.cpp
@@ -1,20 +1,61 @@
-// chap 4 project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-
+#include <iomanip>
+#include <string>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int month, days, year, leap;
+
+    cout << "Enter a month in a number format\n";
+    cin >> month;
+    month >= 1 && month <= 12 ? cout << "Thank you, press enter to continue\n" : cout << "TRY AGAIN\n";
+    cin.ignore(20, '\n');
+    cin.get();
+
+    month >= 1 && month <= 12 ? cout << "Enter a year after 1775\n" : cout << "try again";
+    cin >> year;
+    //year <= 1775 ? cout << "Please retry the program, not valid\n" : cout << "thank you\n"; 
+    if (year <= 1775)
+    {
+        cout << "Please retry the program, not valid\n";
+    }
+    else
+    {
+        cout << "thank you\n\n";
+
+        year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? leap = year, cout << "This is a leap year\n"
+            : cout << "This is not a leap year\n";
+
+
+        switch (month)
+        {
+        case 1: leap == true ? cout << "It is january, there are 31 days\n" : cout << "It is januaray, there are 30 days\n";
+            break;
+        case 2: leap == true ? cout << "It is february, there are 29 days\n" : cout << "It is february, there are 28 days\n";
+            break;
+        case 3: leap == true ? cout << "It is march, there are 31 days\n" : cout << "It is march, there are 30 days\n";
+            break;
+        case 4: leap == true ? cout << "It is april, there are 30 days\n" : cout << "It is april, there are 31 days\n";
+            break;
+        case 5: leap == true ? cout << "It is may, there are 31 days\n" : cout << "It is may, there are 30 days\n";
+            break;
+        case 6: leap == true ? cout << "It is june, there are 30 days\n" : cout << "It is june, there are 31 days\n";
+            break;
+        case 7: leap == true ? cout << "It is july, there are 31 days\n" : cout << "It is july, there are 30 days\n";
+            break;
+        case 8: leap == true ? cout << "It is august, there are 30 days\n" : cout << "It is august, there are 31 days\n";
+            break;
+        case 9: leap == true ? cout << "It is september, there are 31 days\n" : cout << "It is september, there are 30 days\n";
+            break;
+        case 10: leap == true ? cout << "It is october, there are 30 days\n" : cout << "It is october, there are 31 days\n";
+            break;
+        case 11: leap == true ? cout << "It is november, there are 31 days\n" : cout << "It is november, there are 30 days\n";
+            break;
+        case 12: leap == true ? cout << "It is december, there are 30 days\n" : cout << "It is december, there are 31 days\n";
+            break;
+        default: cout << "Not valid\n";
+        }
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
